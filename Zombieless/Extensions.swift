645b19b3b4d69638d8953f07f1bcd6ef7a99bcfb
장용범 <dnwkqk@gmail.com>
2019-06-02 04:03:51 +0900

    upload image

---
+++ Zombieless/Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  Extensions.swift
+//  Zombieless
+//
+//  Created by 장용범 on 31/05/2019.
+//  Copyright © 2019 장용범. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+extension SKSpriteNode {
+    
+    func aspectFillToSize(fillSize: CGSize) {
+        
+        if texture != nil {
+            self.size = texture!.size()
+            
+            let verticalRatio = fillSize.height / self.texture!.size().height
+            let horizontalRatio = fillSize.width /  self.texture!.size().width
+            
+            let scaleRatio = horizontalRatio > verticalRatio ? horizontalRatio : verticalRatio
+            
+            self.setScale(scaleRatio)
+        }
+    }
+    
+}
